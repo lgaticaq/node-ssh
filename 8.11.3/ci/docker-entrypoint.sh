1dd9189d951d42d570ab90eb5e949453fcbd5fbc
Leonardo Gatica <lgatica@protonmail.com>
2018-06-18 18:54:12 -0400

    fix(entrypoint): fix script

--- 8.11.3/ci/docker-entrypoint.sh
+++ 8.11.3/ci/docker-entrypoint.sh
@@ -2,14 +2,9 @@
 
 set +e
 
-if [ -z ${KNOWN_HOST+x} ]; then
-  ssh-keyscan $KNOWN_HOST > /root/.ssh/known_hosts
-fi
-
-if [ -z ${SSH_PRIVATE_KEY+x} ]; then
-  echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa && \
-  chmod 600 /root/.ssh/id_rsa && \
-  echo "    IdentityFile /root/.ssh/id_rsa" >> /etc/ssh/ssh_config
-fi
+ssh-keyscan $KNOWN_HOST > /root/.ssh/known_hosts
+echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa && \
+chmod 600 /root/.ssh/id_rsa && \
+echo "    IdentityFile /root/.ssh/id_rsa" >> /etc/ssh/ssh_config
 
 exec "$@"
